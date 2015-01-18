d43ae191374498c0e3f6afd98e5883df0e2cfc55
Leo McArdle <leomcardle@gmail.com>
2015-01-18 16:29:02 +0000

    added database and simple login/out

---
+++ models/user.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    email: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      set: function(unhashed) {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(unhashed, salt);
+        return this.setDataValue('password', hash);
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var models = require('../models');
 
-/* GET home page. */
+router.use(function(req, res, next) {
+  var links = [{ title: 'Login / Signup', href: '/login' }];
+  if(req.session.email){
+    links = [{ title: 'Logout', href: '/logout' }];
+  }
+  res.render('navbar', { layout: false, links: links }, function(err, html) {
+    req.session.navbar = html;
+    next();
+  });
+});
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { navbar: req.session.navbar, title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { navbar: req.session.navbar });
+});
+
+router.post('/login', function(req, res) {
+  models.User.findOrCreate({
+    where: { email: req.body.email },
+    defaults: { password: req.body.password }
+  }).spread(function(user, created) {
+    if(created) {
+      req.session.email = req.body.email;
+      res.redirect('/');
+    } else {
+      bcrypt.compare(req.body.password, user.values.password, function(err, same){
+        if(same) {
+          req.session.email = req.body.email;
+          res.redirect('/');
+        } else res.redirect('/login');
+      });
+    }
+  });
+});
+
+router.get('/logout', function(req, res) {
+  req.session.destroy(function(err) {
+    res.redirect('back');
+  });
 });
 
 module.exports = router;
