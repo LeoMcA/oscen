f9f212ffc8a199bffff8237df06495136e30ca9d
Leo McArdle <leomcardle@gmail.com>
2015-01-20 11:53:18 +0000

    added adding, deleting, listing, getting and editing songs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var song = require('./routes/song');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(session({
 }));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/song', song);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
